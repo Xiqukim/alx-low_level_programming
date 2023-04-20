1ab0c4b8120b43b75a1f183efe286a57d9fe55fa
Xiqukim <111738682+Xiqukim@users.noreply.github.com>
2023-04-20 16:33:32 +0300

    Create 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+    printf("The size of char is %lu byte(s)\n", sizeof(char));
+    printf("The size of short is %lu byte(s)\n", sizeof(short));
+    printf("The size of int is %lu byte(s)\n", sizeof(int));
+    printf("The size of long is %lu byte(s)\n", sizeof(long));
+    printf("The size of long long is %lu byte(s)\n", sizeof(long long));
+    printf("The size of float is %lu byte(s)\n", sizeof(float));
+    printf("The size of double is %lu byte(s)\n", sizeof(double));
+    printf("The size of long double is %lu byte(s)\n", sizeof(long double));
+    return 0;}
