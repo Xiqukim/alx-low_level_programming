cd511199bb5c7426b0b22818960f7337f38d5a5d
Xiqukim <111738682+Xiqukim@users.noreply.github.com>
2023-04-20 16:25:32 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,2 @@
 #!/bin/bash
-#include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{    puts("Programming is like building");
-    puts("a multilingual puzzle");
-    return (0);}
+#include <stdio.h> int main(void) {puts("Programming is like building a multilingual puzzle\n");return 0;}
